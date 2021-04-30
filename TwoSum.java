c7e5e8d036372783ac4dbc71d1a075c0841389ce
EL AZHARI Mohamed <elazhari.mohamed1@gmail.com>
2021-04-30 17:43:04 -0500

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int index = 0;
+
+        while(index < nums.length - 1){
+            int indexJ = index + 1;
+            while(indexJ < nums.length){
+                if(nums[index] + nums[indexJ] == target){
+                    return new int[]{index, indexJ};
+                }
+                indexJ++;
+            }
+            index ++;
+        }
+        return new int[]{};
+    }
+}
+
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+
+*/
